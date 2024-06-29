1a092fe365fb2a39d5c7959b26d187f94420ea6c
maroinee <maroinelamouchi@gmail.com>
2024-06-29 13:32:46 +0100

    first front

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 import { SignupComponent } from './signup/signup/signup.component';
 import { SigninComponent } from './signin/signin/signin.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'acceuil', component: AcceuilComponent },
   {path:"signup" , component:SignupComponent},
   {path:"signin" , component:SigninComponent},
-  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,21 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importez le NavbarComponent ici
 import { SignupComponent } from './signup/signup/signup.component';
 import { SigninComponent } from './signin/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    AcceuilComponent,
+    NavbarComponent, // Déclarez le NavbarComponent ici
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
